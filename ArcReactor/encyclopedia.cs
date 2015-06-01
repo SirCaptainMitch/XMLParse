0c79d087ec8ded8512b64e12713e7c5bfb0af480
SirCaptainMitch <Hamann.Mitchell@gmail.com>
2015-06-01 15:04:57 -0500

    working without classes

--- ArcReactor/encyclopedia.cs
+++ ArcReactor/encyclopedia.cs
@@ -36,20 +36,17 @@ namespace ArcReactor
                 var spellList = (from r in xmlDoc.Descendants("spell")
                                  select new
                                  {
-                                     Name = r.Element("name").Value,
+                                     Name = r.Element("name").Value.Replace("'", ""),
                                      Level = Convert.ToInt32(r.Element("level").Value),
                                      School = r.Element("school").Value,
                                      Ritual = r.Element("ritual").Value,
                                      Time = r.Element("time").Value,
                                      Range = r.Element("range").Value,
-                                     Components = r.Element("components").Value,
+                                     Components = r.Element("components").Value.Replace("'",""),
                                      Duration = r.Element("duration").Value,
                                      Classes = r.Element("classes").Value,
-                                     Text = r.Elements("text").Select(x => x.Value).ToList()
+                                     Text = r.Elements("text").Select(x => x.Value.Replace("'", "")).ToList()
                                  });
-
-
-
 
                 foreach (var curspell in spellList)
                 {
@@ -77,9 +74,62 @@ namespace ArcReactor
                          curspell.Components,
                          curspell.Duration,
                          curspell.Classes,
-                         String.Join("<br>",curspell.Text.ToArray()) 
+                         String.Join("<br>", curspell.Text.ToArray())
                          );
                 }
+
+            }
+
+            if (fileType == "Race")
+            {
+                var raceList = (from r in xmlDoc.Descendants("race")
+                                select new
+                                {
+                                    Name = r.Element("name").Value.Replace("'", ""),
+                                    Size = r.Element("size").Value,
+                                    Speed = r.Element("speed").Value,
+                                    Ability = r.Element("ability").Value,
+                                    Trait = (from v in r.Elements("trait")
+                                             select new
+                                             {
+                                                 TraitName = v.Element("name").Value.Replace("'", ""),
+                                                 Text = v.Element("text").Value.Replace("'", "")
+                                             }).ToList()
+
+                                });
+
+
+                foreach (var race in raceList)
+                {
+
+                    foreach (var i in race.Trait)
+                    {
+                        s += String.Format(@"
+
+                    INSERT dbo.race (Name, Size, Speed, Ability, Trait, Text)
+                    SELECT
+                            '{0}'
+                            ,'{1}'
+                            ,'{2}'
+                            ,'{3}'
+                            ,'{4}'
+                            ,'{5}'
+                            ;
+                    ",
+                     race.Name,
+                     race.Size,
+                     race.Speed,
+                     race.Ability,
+                     i.TraitName,
+                     i.Text
+                     );
+
+                    }
+
+
+                }
+
+
 
             }
 
